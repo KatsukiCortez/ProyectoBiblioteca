82628cdd5d0f418f2cb4bab652d46fd41dd5bfa3
KatsukiCortez <73317659@continental.edu.pe>
2023-10-30 17:53:36 -0500

    Creación de metodos para la clase libro.

--- src/proyectobiblioteca/Libro.java
+++ src/proyectobiblioteca/Libro.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyectobiblioteca;
 
-/**
- *
- * @author burro
- */
 public class Libro {
+    private String titulo;
+    private String autor;
+    private String genero;
+    private Boolean disponible;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Boolean getDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(Boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+    public Boolean isDisponible(){
+        return disponible;
+    }
+    
+    public boolean prestar(boolean disponible){
+        boolean EsDisponible = false;
+        return EsDisponible;
+    }
+    
+    public boolean devolver(boolean disponible){
+        boolean EsDisponible = true;
+        return EsDisponible;
+    }
     
 }
